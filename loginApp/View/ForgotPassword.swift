5d2b564308cc481fab15cfbf36b5564654bcb805
Henrique Delgado <delgado.ohenrique@gmail.com>
2024-12-05 11:42:45 -0300

    add forgot password view

--- loginApp/View/ForgotPassword.swift
+++ loginApp/View/ForgotPassword.swift
@@ -8,8 +8,84 @@
 import SwiftUI
 
 struct ForgotPassword: View {
+    @State var password: String = ""
+    @State var repeatedPassword: String = ""
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Reset password")
+                .bold()
+                .font(.title2)
+                .hAlign(.leading)
+                .padding(.bottom)
+            Text("Password").foregroundStyle(.secondary).hAlign(.leading)
+            SecureField("", text: $password)
+                .textContentType(.password)
+                .autocapitalization(.none)
+                .padding()
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(Color.gray, lineWidth: 1)
+                )
+
+            Text("At least 8 character and 1 special character")
+                .foregroundStyle(.secondary)
+                .hAlign(.leading)
+                .font(.caption)
+                .padding(.bottom)
+
+            Text("Repeat password").foregroundStyle(.secondary).hAlign(.leading)
+
+            SecureField("", text: $repeatedPassword)
+                .textContentType(.password)
+                .autocapitalization(.none)
+                .padding()
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(Color.gray, lineWidth: 1)
+                )
+
+            Text("Both passwords must match")
+                .foregroundStyle(.secondary)
+                .hAlign(.leading)
+                .padding(.bottom)
+                .font(.caption)
+
+            HStack {
+                Button {
+
+                } label: {
+                    Text("Cancel")
+                        .font(.headline)
+                        .foregroundColor(.black)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 60)
+                        .background(
+                            RoundedRectangle(
+                                cornerRadius: 10
+                            )
+                            .stroke(.gray, lineWidth: 1)
+                        )
+                }.padding(.trailing)
+
+                Button {
+
+                } label: {
+                    Text("Reset")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 60)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                        .shadow(radius: 5)
+                }
+
+            }.padding(.top)
+
+        }.hAlign(.leading).vAlign(.top).padding()
     }
 }
 
---
+++ loginApp/View/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  loginApp
+//
+//  Created by Henrique Delgado on 05/12/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello World!")
+    }
+}
+
+#Preview {
+    HomeView()
+}
