c16e0e693543d3e86585d54a3aa125b4f95a9167
Henrique Delgado <delgado.ohenrique@gmail.com>
2024-12-10 13:40:31 -0300

    create model

---
+++ loginApp/Model/HomeModel.swift
@@ -0,0 +1,7 @@
+//
+//  HomeView.swift
+//  loginApp
+//
+//  Created by Henrique Delgado on 10/12/24.
+//
+
---
+++ loginApp/Model/LoginModel.swift
@@ -0,0 +1,7 @@
+//
+//  LoginModel.swift
+//  loginApp
+//
+//  Created by Henrique Delgado on 09/12/24.
+//
+
